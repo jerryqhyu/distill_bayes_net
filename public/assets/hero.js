9ef7288f9603a1cdfb410a1765051d974ad6b3d8
Jerry Qinghui Yu <sinoforever@outlook.com>
2018-06-20 16:42:08 -0400

    added hero diagram implementation with tfjs

--- public/assets/hero.js
+++ public/assets/hero.js
@@ -1,25 +1,18 @@
 function hero(curve_div, graph_div) {
 
     this.div_id = curve_div.attr('id');
-    this.numLayer = 3;
-    this.depth = 4;
-    this.mu = new Array(this.numLayer + 1);
-    this.sigma = new Array(this.numLayer + 1);
-    for (var i = 0; i < this.mu.length; i++) {
-        if (i == 0 || i == this.mu.length-1) {
-            this.mu[i] = tf.randomNormal([this.depth, 1]);
-            this.sigma[i] = tf.randomNormal([this.depth, 1]);
-        } else {
-            this.mu[i] = tf.randomNormal([this.depth, this.depth]);
-            this.sigma[i] = tf.randomNormal([this.depth, this.depth]);
-        }
-    }
+
+    const shape = tf.tensor1d([1, 5, 5, 1], 'int32');
+    const w = tf.variable(tf.randomNormal(shape.slice([0], [shape.shape[0] - 1]).mul(shape.slice([1])).sum().flatten().dataSync()));
+    const w_v = tf.variable(tf.randomNormal(shape.slice([0], [shape.shape[0] - 1]).mul(shape.slice([1])).sum().flatten().dataSync()));
+    const b = tf.variable(tf.randomNormal(shape.slice([1]).sum().flatten().dataSync()));
+    const b_v = tf.variable(tf.randomNormal(shape.slice([1]).sum().flatten().dataSync()));
 
     this.start = function() {
         if (!training_interval) {
             console.log("started training");
-            training_interval = d3.timer(train_epoch, 50);
-            plot_interval = d3.timer(plot, 200);
+            training_interval = d3.timer(train, 50);
+            plot_interval = d3.timer(plot, 50);
         }
     }
 
@@ -37,45 +30,65 @@ function hero(curve_div, graph_div) {
     }
 
     this.reset = function() {
-        samples = sample_from_seed("Toronto", 10, 144);
-        net = make_preset_net();
-        trainer = new net_lib.Trainer(net, {
-            learning_rate: param.learning_rate * 2,
-            batch_size: param.validation_points.length * 2
-        });
-        plot();
-        this.stop();
-        epoch_count = 0;
+
     }
 
-    this.predict = function(data, weights) {
-        var x = data;
-        for (var i = 0; i < weights.length; i++) {
-            x = weights.matMul(x).leakyRelu(0.1);
-        }
+    function predict(x, sample) { 
+        return tf.tidy(() => {
+            const unpacked = unpack_sample(sample);
+            for (var i = 0; i < unpacked.weights.length; i++) {
+                x = unpacked.weights[i].matMul(x).elu(0.5).add(unpacked.biases[i]);
+            }
+            return x;
+        });
     }
 
     var curve_plotter = new Plotter(curve_div, param.curve_domain_x_extended, param.curve_domain_y, false, false);
     var graph_plotter = new Plotter(graph_div, param.nn_domain, param.nn_domain, false, false);
 
     //define a neural network
-    var epoch_count = 0;
-    var model = bayes_net();
     var loss = (pred, label) => pred.sub(label).square().mean();
-    var samples = sample_from_seed("Toronto", 10, 144);
 
     //interval controller
     var training_interval;
     var plot_interval;
     setup();
 
-    function train_epoch() {
-        var x;
-        for (var j = 0; j < param.validation_points.length; j++) {
-            x = new net_lib.Vol([param.validation_points[j]]);
-            trainer.train(x, [Math.sin(param.validation_points[j]) + param.validation_noise[j]]);
+    function train() {
+        const optimizer = tf.train.sgd(param.learning_rate);
+        for (let iter = 0; iter < train_xs.length; iter++) {
+            const noise_w = tf.randomNormal([w_v.shape[0]]);
+            const noise_b = tf.randomNormal([b_v.shape[0]]);
+            const sample = {
+                weights: w_v.mul(noise_w).add(w),
+                biases: b,
+            }
+            // const sample = {
+            //     weights: weights_dist.w,
+            //     biases: weights_dist.b,
+            // }
+            optimizer.minimize(() => {
+                const predsYs = predict(tf.tensor2d(train_xs[iter], [1, 1]), sample);
+                return loss(predsYs, tf.tensor1d(train_ys[iter]));
+            });
         }
-        epoch_count++;
+    }
+
+    function unpack_sample(sample) {
+        var w_ind = 0;
+        var b_ind = 0;
+        var unpacked = {
+            weights: [],
+            biases: []
+        };
+		var size_arr = shape.dataSync();
+        for (var i = 0; i < shape.shape[0] - 1; i++) {
+            unpacked.weights.push(sample.weights.slice([w_ind], [size_arr[i] * size_arr[i + 1]]).reshape([size_arr[i + 1], size_arr[i]]));
+            unpacked.biases.push(sample.biases.slice([b_ind], [size_arr[i + 1]]).reshape([size_arr[i + 1], 1]));
+			w_ind += size_arr[i] * size_arr[i + 1];
+			b_ind += size_arr[i + 1];
+        }
+        return unpacked;
     }
 
     function setup() {
@@ -89,14 +102,29 @@ function hero(curve_div, graph_div) {
 
     function plot() {
         plot_path();
-        graph_plotter.plot_neural_net(net, "#float");
+        // graph_plotter.plot_neural_net(net, "#float");
     }
 
     function plot_path() {
-        var curve = variational_prediction(curve_x_extended);
-        curve_plotter.plot_path(curve, {
+        const seed =  [1, 2, 3, 4, 5];
+        const samples = seed.map(s => {
+            return {
+                weights: w_v.mul(tf.randomNormal([w_v.shape[0]], 0, 1, 'float32', s)).add(w),
+                biases: b_v.mul(tf.randomNormal([b_v.shape[0]], 0, 1, 'float32', s)).add(b),
+            }
+        })
+        // const sample = {
+        //     weights: w,
+        //     biases: b,
+        // }
+        var curves = samples.map(sample => {
+            return curve_x_extended.map(x => {
+                return {x: x, y: predict(tf.tensor2d([x], [1, 1]), sample).dataSync()[0]};
+            })
+        });
+        curve_plotter.plot_path(curves, {
             color: "darkorange",
-            width: 1,
+            width: 3,
             opacity: 0.5,
             id: "#float"
         });
@@ -110,25 +138,5 @@ function hero(curve_div, graph_div) {
             opacity: 0.5,
             id: "#fixed"
         });
-    }
-
-    function variational_prediction(x) {
-        var predicted_values;
-        var x_val;
-        var pred = new Array(samples.length);
-        for (var i = 0; i < samples.length; i++) {
-            pred[i] = new Array(x.length);
-        }
-        for (var i = 0; i < x.length; i++) {
-            x_val = new net_lib.Vol([x[i]]);
-            predicted_values = net.variationalForward(x_val, samples);
-            for (var j = 0; j < samples.length; j++) {
-                pred[j][i] = {
-                    x: x[i],
-                    y: predicted_values[j].w[0]
-                };
-            }
-        }
-        return pred;
     }
 }
